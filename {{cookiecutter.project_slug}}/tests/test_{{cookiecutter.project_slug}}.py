04ded834bc8169085332de04046b37df021527f1
Tom Hill <tom@clockworkcubed.com>
2018-01-08 17:44:50 +0000

    Initial commit

---
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -0,0 +1,20 @@
+import unittest
+from {{cookiecutter.project_slug}} import {{cookiecutter.project_slug}}
+
+
+class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
+    """Tests for `{{ cookiecutter.project_slug }}` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_function_returns_true(self):
+        """Test function returns true."""
+        self.assertTrue({{cookiecutter.project_slug}}.function())
+
+
+if __name__ == '__main__':
+    unittest.main()
